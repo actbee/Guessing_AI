0126d11647ef2d0c7c8489ca2d23379305068792
actbee <rainman511@outlook.com>
2022-04-28 18:25:08 -0400

    updated bayes network

--- src/components/naiveBayes/naiveBayes.jsx
+++ src/components/naiveBayes/naiveBayes.jsx
@@ -1,7 +1,27 @@
-const trainNaiveBayes = (imageArray) => {
-	return placeholderWeights
-}
+<script src="https://unpkg.com/ml5@0.4.3/dist/ml5.min.js"></script>
+// npm install bayes
+var bayes = require('bayes')
+​
+var classifier = bayes()
+​
+const processing_classifier = ml5.imageClassifier("MobileNet", modelLoaded);​
 
-const runNaiveBayes = (image) => {
-	return true; // Placeholder true/false
+
+// Takes in objects detected in images as one string, and user label
+function trainNaiveBayes(imagearray, userLabel) {
+	var imageLabels = [];
+	for (var i =0; i<imagearray.length; i++){
+		processing_classifier.predict(imagearray[i], 
+			function (err, results) {
+				  imageLabels.append(results[0].label);
+				});
+	}
+
+    classifier.learn(imageLabels, userLabel)
 }
+​
+// Takes in objects in image, outputs user label
+function predictLabel(imageLabels) {
+    return classifier.categorize(imageLabels)
+}
+​
