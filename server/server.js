23719933796885e4df8bc6ca939d88a6859eb3d5
actbee <rainman511@outlook.com>
2022-05-10 16:28:19 -0400

    deployed on heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.listen(port, () =>{
-    console.log('Server is up on port ${port}!');
+    console.log('Server is up !');
 });
 
 app.get('*', (req, res) => {
